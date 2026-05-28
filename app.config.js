33baba09ba87f032014087d1d113ad6d20eba450
kiranfin <4finjas@online.de>
2026-05-28 21:48:51 +0200

    Added dev build

---
+++ app.config.js
@@ -0,0 +1,67 @@
+const IS_DEV = process.env.APP_VARIANT === "development";
+
+export default {
+    expo: {
+        name: IS_DEV ? "MyTTR Tracker Dev" : "MyTTR Tracker",
+        slug: "tttracker",
+        version: "1.0.0",
+        orientation: "portrait",
+        icon: "./assets/images/icon.png",
+        scheme: IS_DEV ? "tttracker-dev" : "tttracker",
+        userInterfaceStyle: "automatic",
+        newArchEnabled: true,
+
+        ios: {
+            supportsTablet: true,
+            bundleIdentifier: IS_DEV
+                ? "com.kiranfin.tttracker.dev"
+                : "com.kiranfin.tttracker",
+        },
+
+        android: {
+            adaptiveIcon: {
+                foregroundImage: "./assets/images/icon.png",
+                backgroundColor: "#013762",
+            },
+            edgeToEdgeEnabled: true,
+            predictiveBackGestureEnabled: false,
+            package: IS_DEV
+                ? "com.kiranfin.tttracker.dev"
+                : "com.kiranfin.tttracker",
+        },
+
+        web: {
+            output: "static",
+            favicon: "./assets/images/favicon.png",
+        },
+
+        plugins: [
+            "expo-router",
+            [
+                "expo-splash-screen",
+                {
+                    image: "./assets/images/android-icon-foreground.png",
+                    imageWidth: 200,
+                    resizeMode: "contain",
+                    backgroundColor: "#005F92",
+                    dark: {
+                        backgroundColor: "#002143",
+                    },
+                },
+            ],
+            "expo-secure-store",
+        ],
+
+        experiments: {
+            typedRoutes: true,
+            reactCompiler: true,
+        },
+
+        extra: {
+            router: {},
+            eas: {
+                projectId: "82fe0152-f251-4543-859d-2afe28d80d5d",
+            },
+        },
+    },
+};
